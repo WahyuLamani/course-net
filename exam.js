f6fbc5b5b79e3d18b74c0f11e76497be29f64c56
wahyulamani <razorwahyu1@gmail.com>
2024-06-08 13:58:47 +0800

    exam selesai

--- exam.js
+++ exam.js
@@ -1,12 +1,33 @@
-const args = process.argv[2];
-if (args.length > 0) {
-    // Menggabungkan argumen menjadi satu string
-    console.log(
-        `String yang diambil: ${args}
-----------------------
-`
-
-    );
+const input = (process.argv[2] ?? '').toLowerCase()
+if (input.length > 0) {
+    console.log(hitungVokal(input));
 } else {
-    console.log('Tidak ada string yang diberikan sebagai argumen.');
+    console.log("Tidak ada input string yang ditambahkan");
+}
+function hitungVokal(string) {
+    let objVokal = {
+        A: 0,
+        I: 0,
+        U: 0,
+        E: 0,
+        O: 0
+    }
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === 'a') {
+            objVokal.A++
+        }
+        if (string[i] === 'i') {
+            objVokal.I++
+        }
+        if (string[i] === 'u') {
+            objVokal.U++
+        }
+        if (string[i] === 'e') {
+            objVokal.E++
+        }
+        if (string[i] === 'o') {
+            objVokal.O++
+        }
+    }
+    return objVokal
 }
---
+++ exam/hitungVokalSempurna.js
@@ -0,0 +1,40 @@
+const input = process.argv[2] ?? ''
+if (input.length > 0) {
+    console.log("\nKalimat: "+input+"\n \nJumlah Huruf Vokal\n------------------")
+    const hasil =  hitungVokal(input)
+    let countHasil = 0
+        for (const prop in hasil) {
+            console.log(`${prop}: ${hasil[prop]}`);
+            countHasil += hasil[prop]
+        }
+        console.log(`Total: ${countHasil}`);
+} else {
+    console.log("Tidak ada input string yang ditambahkan");
+}
+function hitungVokal(string) {
+    let objVokal = {
+        A: 0,
+        I: 0,
+        U: 0,
+        E: 0,
+        O: 0
+    }
+    for (let i = 0; i < string.length; i++) {
+        if (string[i].toLowerCase() === 'a') {
+            objVokal.A++
+        }
+        if (string[i].toLowerCase() === 'i') {
+            objVokal.I++
+        }
+        if (string[i].toLowerCase() === 'u') {
+            objVokal.U++
+        }
+        if (string[i].toLowerCase() === 'e') {
+            objVokal.E++
+        }
+        if (string[i].toLowerCase() === 'o') {
+            objVokal.O++
+        }
+    }
+    return objVokal
+}
